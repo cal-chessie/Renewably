9128e6041784add85c87f37f65581b2cfbb12380
cal-chessie <cal-chessie@users.noreply.github.com>
2026-04-20 14:31:45 +0000

    chore: remove upload/, supabase-migrations/, and remaining misc files (generate-*, watchdog, start.sh, supabase SQL)

--- upload/AISolar-main/AISolar-main/src/components/ai-analyser/index.ts
+++
@@ -1,5 +0,0 @@
-export { AIBillAnalyser } from "./AIBillAnalyser";
-export { BillInputStep } from "./BillInputStep";
-export { AnalysisResultsStep } from "./AnalysisResultsStep";
-export { LeadCaptureModal } from "./LeadCaptureModal";
-export { SoftCTAStep } from "./SoftCTAStep";
--- upload/AISolar-main/AISolar-main/src/components/landing/index.ts
+++
@@ -1,9 +0,0 @@
-export { WhatIsSection } from './WhatIsSection';
-export { IsSolarWorthItSection } from './IsSolarWorthItSection';
-export { SavingsSection } from './SavingsSection';
-export { HowItWorksSection } from './HowItWorksSection';
-export { WhyUsSection } from './WhyUsSection';
-export { FAQSection, faqStructuredData } from './FAQSection';
-export { StickyCTA } from './StickyCTA';
-export { MobileHomeNav } from './MobileHomeNav';
-export { ExpertChat } from './ExpertChat';
