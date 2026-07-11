39d4b55c9a69ac3c346ed94e57f4dbf6cce584dc
Cal <cal@renewably.ie>
2026-07-11 03:23:59 +0100

    Fix syntax error in script-src directive

--- next.config.ts
+++ next.config.ts
@@ -96,7 +96,7 @@ const nextConfig: NextConfig = {
                 ].join("; ")
               : [
                   "default-src 'self'"
-                "script-src 'self' 'unsafe-inline' https://js.stripe.com",,
+                "script-src 'self' 'unsafe-inline' https://js.stripe.com",
                   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                   "font-src 'self' https://fonts.gstatic.com",
                   "img-src 'self' data: blob: https:",
