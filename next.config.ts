19460223a4d0c4f217caa93d5ea95ac84bddb3e7
Cal <cal@renewably.ie>
2026-07-11 03:21:40 +0100

    Update Content Security Policy for script-src

--- next.config.ts
+++ next.config.ts
@@ -95,8 +95,8 @@ const nextConfig: NextConfig = {
                   "form-action 'self' https:",
                 ].join("; ")
               : [
-                  "default-src 'self'",
-                  "script-src 'self' https://js.stripe.com",
+                  "default-src 'self'"
+                "script-src 'self' 'unsafe-inline' https://js.stripe.com",,
                   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                   "font-src 'self' https://fonts.gstatic.com",
                   "img-src 'self' data: blob: https:",
